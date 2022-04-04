a7de8969d7421309c57b25ab66e2f659cfea9ce3
kovalski-rgb <andre.kovalski@tecpuc.com.br>
2022-04-03 22:48:58 -0300

    Levantamento de exceções e melhora na legibilidade do codigo

--- entity/List.java
+++ entity/List.java
@@ -20,7 +20,7 @@ public class List {
 	}
 	
 	/**
-	 * Searches for a given value, and return in which element it exists
+	 * Searches for a given value, and return the element where it is contained
 	 * @param data = value that will be searched for
 	 * @return Element that contains such data
 	 */
@@ -36,11 +36,27 @@ public class List {
 	}
 	
 	/**
-	 * Inserts value into first of the list
+	 * Looks for a given value inside the list
+	 * @param data = value that will be looked for
+	 * @return true if given value exists, otherwise, false
+	 */
+	public boolean contains(int data) {
+		Element aux = element;
+		while(data != aux.getData()) {
+			aux = aux.getForwardElement();
+			if(aux == null) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Inserts value into first position of the list
 	 * @param data = data that will be inserted in the first position of the list
 	 */
 	public void insertInFirst(int data) {
-		if(element == null) {
+		if(isEmpty()) {
 			element = new Element(data);
 			return;
 		} else {
@@ -56,7 +72,7 @@ public class List {
 	 * @param data = data that will be inserted in the last position of the list
 	 */
 	public void insertInLast(int data) {
-		if(element == null) {
+		if(isEmpty()) {
 			element = new Element(data);
 			return;
 		}
@@ -76,16 +92,10 @@ public class List {
 	 * @param newData = Data that will be inserted after the specified value
 	 */
 	public void insertAfterElement(int oldData, int newData) {
-		if(element == null) {
+		if(isEmpty()) {
 			throw new RuntimeException("Cannot insert ("+newData+") after ("+oldData+"), the list is empty!");
 		}
-		Element aux = element;
-		while(aux.getData() != oldData) {
-			aux = aux.getForwardElement();
-			if(aux == null) {
-				throw new RuntimeException("List does not contain such value: ("+oldData+')');
-			}
-		}
+		Element aux = find(oldData);
 		Element newAux = new Element(newData);
 		newAux.setForwardElement(aux.getForwardElement());
 		newAux.setBackwardElement(aux);
@@ -100,21 +110,25 @@ public class List {
 	 * @param data = value that will be removed
 	 */
 	public void remove(int data) {
-		Element aux = element;
-		while(aux.getData() != data) {
-			aux = aux.getForwardElement();
-			if(aux == null) {
-				throw new RuntimeException("List does not contain such value: ("+data+')');
-			}
+		if(isEmpty()) {
+			throw new RuntimeException("List is empty");
 		}
-		aux.getBackwardElement().setForwardElement(aux.getForwardElement());
-		aux.getForwardElement().setBackwardElement(aux.getBackwardElement());
+		Element aux = find(data);
+		if(aux.getBackwardElement() != null) {
+			aux.getBackwardElement().setForwardElement(aux.getForwardElement());
+		}
+		if(aux.getForwardElement() != null) {
+			aux.getForwardElement().setBackwardElement(aux.getBackwardElement());
+		}
 	}
 
 	/**
 	 * Removes last element from list
 	 */
 	public void removeLastElement() {
+		if(isEmpty()) {
+			throw new RuntimeException("List is empty");
+		}
 		Element aux = element;
 		while(aux.getForwardElement() != null) {
 			aux = aux.getForwardElement();
@@ -126,6 +140,9 @@ public class List {
 	 * Removes first element from list
 	 */
 	public void removeFirstElement() {
+		if(isEmpty()) {
+			throw new RuntimeException("List is empty");
+		}
 		element = element.getForwardElement();
 	}
 	
@@ -135,7 +152,7 @@ public class List {
 	public void clearList() {
 		element = null;
 	}
-
+	
 	/**
 	 *  Prints all elements chained into {@link #element} parameter from beggining to end
 	 */
--- program/Main.java
+++ program/Main.java
@@ -8,45 +8,45 @@ public class Main {
 		
 	    List lista = new List();
 	    
-	    // exemplos de inserção por primeiro (no estilo pilha)
+	    // exemplos de inserção por primeiro
 	    // com verificação da lista vazia, e print
 	    System.out.println("Is it empty? "+lista.isEmpty());
+	    lista.print();
 	    lista.insertInFirst(3);
 	    lista.insertInFirst(2);
 	    lista.insertInFirst(1);
-	    lista.print();
 	    System.out.println("Is it empty? "+lista.isEmpty());
+	    lista.print();
 	    System.out.println();
 	    
 	    // inserção no ultimo elemento
+	    System.out.println("Inserir 4 no final da lista");
 	    lista.insertInLast(4);
 	    lista.print();
+	    System.out.println();
 	    
 	    // inserção do 5 após o dado 4
 	    // com print do inicio ao fim, e do fim ao inicio
+	    System.out.println("Inserir 5 após o 4, e 6 após o 5");
 	    lista.insertAfterElement(4, 5);
+	    lista.insertAfterElement(5, 6);
 	    lista.print();
 	    lista.reversePrint();
+	    System.out.println();
 	    
 	    // remoção do primeiro, e ultimo elemento
 	    // além do elemento com dado 3
+	    System.out.println("Remove o primeiro, ultimo, o dado 3 e o dado 5");
 	    lista.removeFirstElement();
 	    lista.removeLastElement();
 	    lista.remove(3);
+	    lista.remove(5);
 	    
 	    lista.print();
 
 	    lista.clearList();
-	    
 	    System.out.println();
-	    lista.insertInFirst(1);
-	    lista.insertInLast(2);
 	    lista.print();
-	    
-	    // procura de elementos na lista, com retorno do elemento procurado
-	    System.out.println("Elemento1: "+lista.find(1));
-	    System.out.println("Elemento2: "+lista.find(2));
-
 	}
 
 }
